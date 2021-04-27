c8b1f2af147512025c8b23014e7c2d57378f06bf
sun167 <caoson167@gmail.com>
2021-04-27 17:06:49 +0200

    Add delete buttons

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+let table = document.getElementById("table");
 function Book(title, author, genre){
     this.title = title;
     this.author = author;
     this.genre = genre;
 }
 
-const book1 = new Book("1","a",";");
-const book2 = new Book("2","b",".");
-
-let myLibrary = [book1, book2];
+let myLibrary = [];
 function show(){            
     var result = "";
+    let counter = 0;
     myLibrary.forEach(book => {
-        result = "";
-        result += "<td>"+ book.title + "</td>";
-        result += "<td>"+ book.author + "</td>";
-        result += "<td>"+ book.genre + "</td>";
-        document.getElementById("table").appendChild(document.createElement("tr")).innerHTML = result;
+        result = 
+        `<td> ${book.title} </td>
+        <td> ${book.author} </td>
+        <td> ${book.genre} </td>
+        <td><button type="submit">Delete</button></td>`;
+        let newBook = document.createElement("tr");
+        newBook.className = `${counter}`;
+        table.appendChild(newBook).innerHTML = result;
+        newBook.addEventListener("click",deleteBook);
+        counter++;
+
     });
 }
 function addBook(event){
@@ -25,14 +30,30 @@ function addBook(event){
     let genre = document.querySelectorAll(".genre")[0].value;
     let newBook = new Book(title,author,genre);
     myLibrary.push(newBook);
+    console.log(myLibrary);
     update();
 }
+
+function deleteBook(event){
+    event.preventDefault();
+    let reponse = window.confirm("Are you sure?");
+    if (reponse) {
+    table.removeChild(event.currentTarget);
+    let bookToDelete = event.currentTarget.className;
+    console.log(bookToDelete);
+    console.log(event.currentTarget);
+    myLibrary.splice(bookToDelete,1);
+    update();
+    }
+}
+
 function update(){
     document.getElementById("table").innerHTML = 
         `<tr>
             <td>Title</td>
             <td>Author</td>
             <td>Genre</td>
+            <td></td>
         </tr>`;
     show();
 }
